3d18cc85fe998a50524e1f57d4e95255a52653ed
osmond1689 <osmond1689@gmail.com>
2021-07-16 00:05:32 +0800

    243

--- study.c
+++ study.c
@@ -1,12 +1,39 @@
 #include <stdio.h>/*默认库*/
 #include <string.h>
+#include <ctype.h> //判断类型库，判断通过返回真失败返回假
+/*
+isalnum() 字母数字
+isalpha() 字母
+isablank() 标准的空白字符
+iscntrl() 控制字符
+isdigit() 数字
+isgraph() 除空格以为可打印的字符
+islower() 小写字母
+isprint() 可打印字符
+ispunct() 标点符合
+isspace() 空白字符
+isupper() 大写字符
+isxdigit() 十六进制数字符
+tolower() 转小写
+toupper() 转大写
+*/
 #define NAME "osmond"//预处理器 别名 尽量全大写
 
 void test();//声明原型
 void scanf_test();
 void string_test();
 void loop_test();
-
+void if_test();
+void switch_test();
+void io_test();
+/*
+使用iso646.h 
+可以使用and or not
+逻辑运算符 
+与 &&
+或 ||
+非！
+*/
 /*
 int,默认10进制%d，0x 16进制%x，08进制%o
 long,
@@ -27,7 +54,10 @@ int main(void)//默认方法格式，void 无返回值
     printf("hello world %d\n",num);//要加分号
     //test();//调用方法
     //scanf_test();
-    loop_test();
+    //loop_test();
+    //if_test();
+    //switch_test();
+    io_test();
     return 0;
 }
 
@@ -38,12 +68,14 @@ void test(void)
 void scanf_test(void)
 {
     char i_test[3];
+    char ch;
     scanf("%s",i_test);//获取输入，存入i_test变量，写入内存地址，&取地址符 字符串名称就是地址
     printf("%s",i_test);
     getchar();//读取下一个输入字符
     printf("test %s\n",i_test);
     printf("%zd",sizeof(i_test));//以字节为单位,zd格式化，或u，lu，返回size_t类型 无符合整数
-    string_test();
+    ch=getchar();//获取输入字符
+    putchar(ch);//输出字符，只能处理char
 }
 
 void string_test(void)
@@ -67,7 +99,7 @@ void loop_test(void)//void可省略,无参数
     for (int index=0;index<2;index++)//for循环 执行一次；条件语句；每次循环结束都执行
     {
         printf("循环%d次",index);
-        continue;
+        continue;//跳出本次循环
     }
     int i=0;
     for (printf("test");i<2;printf("test"))//for test
@@ -82,7 +114,57 @@ void loop_test(void)//void可省略,无参数
     while(i<2);
 }
 
-void array_test()//数组，字符数组+\0=字符串
+void if_test(void)
 {
-    
+    int i=0;
+    if (i<1)//if
+    {
+        printf("小于1");
+    }
+    else if(i>1)
+    {
+        printf("大于1");
+    }
+    else 
+    {
+        printf("出现意外");
+    }
+    /*运算符？：*/
+    i=(i=0)?1:2;
+    printf("%d",i);
 }
+
+void switch_test()
+{
+    test_title://goto 测试
+    switch (1)//swicth
+    {
+    case 1/* constant-expression */:
+        /* code */
+        printf("test");
+        break;
+    case 2:
+        break;
+    
+    default:
+        break;
+    }
+    goto test_title;//goto 测试
+    //goto
+}
+
+void io_test()
+{   
+    char ch;
+    while ((ch=getchar())!=EOF)//末尾会检测到EOF，EOF是一个值
+    {
+        printf("持续");
+    }
+    printf("结束");
+}
+
+
+// void array_test()//数组，字符数组+\0=字符串
+// {
+    
+// }
